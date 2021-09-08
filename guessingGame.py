d13c51aa8d0466c80713cf0069a668f20e5b614c
NikithaChandrashekar <72256727+NikithaChandrashekar@users.noreply.github.com>
2021-09-08 17:21:41 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,11 @@
+
+name=input("Hi!Enter your name.")
+print("ok"+name+",hi!")
+age=input("Please tell me your age!")
+print("hmm,okay !")
+q1=input("guess the capital of india!")
+if(q1=="Delhi"):
+    print("yes that is correct!")
+else:
+    print("sorry, but that is wrong.")
+
